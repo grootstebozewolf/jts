12e0f27f294a625b09fb66ea7bafc39a605514bf
Jeroen Bloemscheer <jeroen.bloemscheer@merkator.com>
2026-07-11 23:01:38 +0200

    test: add CircularString toShape exercise in PolygonShapeTest (drives real arc-to-bezier path for verification)

--- modules/core/src/test/java/org/locationtech/jts/awt/PolygonShapeTest.java
+++ modules/core/src/test/java/org/locationtech/jts/awt/PolygonShapeTest.java
@@ -34,4 +34,31 @@ public class PolygonShapeTest extends GeometryTestCase {
     Geometry geomExpected = read("POLYGON ((100 -200, 200 -200, 200 -100, 100 -100, 100 -200))");
     assertTrue(geomExpected.equalsExact(geom2));
   }
+
+  public void testCircularStringToShapeArc() {
+    // Drives the real shipped ShapeWriter.toShape(CircularString) code path
+    GeometryFactory gf = new GeometryFactory();
+    CircularString cs = gf.createCircularString(new Coordinate[] {
+      new Coordinate(0, 0),
+      new Coordinate(10, 10),
+      new Coordinate(20, 0)
+    });
+    ShapeWriter sw = new ShapeWriter();
+    Shape shp = sw.toShape(cs);
+    assertNotNull("toShape must return non-null Shape for CircularString arc", shp);
+
+    // Walk the path to prove the arc logic (cubic or line) was executed
+    PathIterator pi = shp.getPathIterator(null);
+    int segCount = 0;
+    int cubicCount = 0;
+    float[] buf = new float[6];
+    while (!pi.isDone()) {
+      int type = pi.currentSegment(buf);
+      segCount++;
+      if (type == PathIterator.SEG_CUBICTO) cubicCount++;
+      pi.next();
+    }
+    System.out.println("CircularString toShape exercised: segs=" + segCount + " cubics=" + cubicCount);
+    assertTrue("must have produced path segments", segCount > 0);
+  }
 }
