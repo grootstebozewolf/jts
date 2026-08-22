c498715eb627964c0ac4d64b648b7a83a4f50ebc
Jeroen Bloemscheer <jeroen.bloemscheer@merkator.com>
2026-08-22 11:34:02 +0200

    fix: EditVertex right-click add on CIRCULARSTRING keeps odd control count (#82)
    Inserting one control made an even CircularString (19 -> 20 on the UX
    GEOMETRYCOLLECTION witness). Split the clicked arc into two (net +2)
    so the type stays CIRCULARSTRING and WKT still re-parses.

--- modules/app/src/main/java/org/locationtech/jtstest/testbuilder/geom/GeometryVertexInserter.java
+++ modules/app/src/main/java/org/locationtech/jtstest/testbuilder/geom/GeometryVertexInserter.java
@@ -15,6 +15,7 @@ package org.locationtech.jtstest.testbuilder.geom;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.curve.CircularString;
 import org.locationtech.jts.geom.util.GeometryEditor;
 
 public class GeometryVertexInserter 
@@ -47,7 +48,10 @@ public class GeometryVertexInserter
         Geometry geometry)
     {
       if (geometry != line) return coords;
-      
+      if (geometry instanceof CircularString) {
+        return insertOnCircularString(coords);
+      }
+
       Coordinate[] newPts = new Coordinate[coords.length + 1];
       for (int i = 0; i < coords.length; i++) {
         int actualIndex = i > segIndex ? i + 1 : i;
@@ -55,6 +59,45 @@ public class GeometryVertexInserter
       }
       newPts[segIndex + 1] = (Coordinate) newVertex.clone();
       return newPts;
+    }
+
+    /**
+     * A CircularString control count must stay odd and >= 3. Inserting one
+     * point (LineString behaviour) makes an even sequence that is not a
+     * valid CircularString. Split the clicked arc (A, M, B) at the new
+     * vertex C into (A, mid(A,C), C) and (C, mid(C,B), B): net +2.
+     */
+    private Coordinate[] insertOnCircularString(Coordinate[] coords)
+    {
+      int arcStart = (segIndex / 2) * 2;
+      if (arcStart + 2 >= coords.length) {
+        return coords;
+      }
+      Coordinate a = coords[arcStart];
+      Coordinate b = coords[arcStart + 2];
+      Coordinate c = (Coordinate) newVertex.clone();
+      Coordinate m1 = midpoint(a, c);
+      Coordinate m2 = midpoint(c, b);
+
+      Coordinate[] newPts = new Coordinate[coords.length + 2];
+      int j = 0;
+      for (int i = 0; i < arcStart; i++) {
+        newPts[j++] = (Coordinate) coords[i].clone();
+      }
+      newPts[j++] = (Coordinate) a.clone();
+      newPts[j++] = m1;
+      newPts[j++] = c;
+      newPts[j++] = m2;
+      newPts[j++] = (Coordinate) b.clone();
+      for (int i = arcStart + 3; i < coords.length; i++) {
+        newPts[j++] = (Coordinate) coords[i].clone();
+      }
+      return newPts;
+    }
+
+    private static Coordinate midpoint(Coordinate p0, Coordinate p1)
+    {
+      return new Coordinate((p0.x + p1.x) / 2.0, (p0.y + p1.y) / 2.0);
     }
   }
 
